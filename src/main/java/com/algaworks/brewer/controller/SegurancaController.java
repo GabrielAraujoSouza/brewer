6a927aa52a073362587a4fbc398680319510b814
Gabriel Araujo de Souza <souzaa.gabriel@gmail.com>
2018-04-09 18:44:16 -0300

    19.8. Entendendo o CSRF

--- src/main/java/com/algaworks/brewer/controller/SegurancaController.java
+++ src/main/java/com/algaworks/brewer/controller/SegurancaController.java
@@ -9,10 +9,17 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class SegurancaController {
 
 	@GetMapping("/login")
-	public String login(@AuthenticationPrincipal User user){
-		if(user != null){
+	public String login(@AuthenticationPrincipal User user) {
+		if (user != null) {
 			return "redirect:/cervejas";
 		}
+		
 		return "Login";
 	}
+	
+	@GetMapping("/403")
+	public String acessoNegado() {
+		return "403";
+	}
+
 }
